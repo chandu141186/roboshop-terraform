37b426f0400b43c4261f3222a8474ca1b7b15ba6
chandu141186 <chandu141186@gmail.com>
2024-07-21 17:00:33 +0530

    aws vpc

---
+++ 01-vpc/main.tf
@@ -0,0 +1,18 @@
+module "roboshop" {
+  #source = "../terraform-aws-vpc"
+  source = "git::https://github.com/chandu141186/terraform-aws-vpc.git?ref=main"
+  project_name = var.project_name 
+  environment = var.environment 
+  common_tags = var.common_tags
+  vpc_tags = var.vpc_tags
+
+ #public_subnet {
+#  public_subnets_cidr = "var.public_subnets_cidr"
+#  }
+#   private_subnet {
+#  private_subnets_cidr = " var.private_subnets_cidr"
+#   }
+#private_subnet {
+#  private_subnets_cidr = " var.private_subnets_cidr"
+#   }
+}
