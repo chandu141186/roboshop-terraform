5179edfe2e192988580d43505f5aedd110eb4f0a
chandu141186 <chandu141186@gmail.com>
2024-07-23 16:32:21 +0530

    terrafrom

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -1,23 +1,23 @@
 
- module "vpn" {
-  source         = "../../terraform-aws-securitygroup"
-  project_name   = var.project_name
-  environment    = var.environment
-  sg_description = "SG for VPN"
-  vpc_id         = data.aws_vpc.default.id
-  sg_name        = "vpn"
-  #sg_ingress_rules = var.mongodb_sg_ingress_rules
-}
+#  module "vpn" {
+#   source         = "../../terraform-aws-securitygroup"
+#   project_name   = var.project_name
+#   environment    = var.environment
+#   sg_description = "SG for VPN"
+#   vpc_id         = data.aws_vpc.default.id
+#   sg_name        = "vpn"
+#   #sg_ingress_rules = var.mongodb_sg_ingress_rules
+# }
 
-module "mongodb" {
-  source         = "../../terraform-aws-securitygroup"
-  project_name   = var.project_name
-  environment    = var.environment
-  sg_description = "SG for MongoDB"
-  vpc_id         = data.aws_ssm_parameter.vpc_id.value
-  sg_name        = "mongodb"
-  #sg_ingress_rules = var.mongodb_sg_ingress_rules
-}
+ module "mongodb" {
+   source         = "../../terraform-aws-securitygroup"
+   project_name   = var.project_name
+   environment    = var.environment
+   sg_description = "SG for MongoDB"
+   vpc_id         = data.aws_ssm_parameter.vpc_id.value
+   sg_name        = "mongodb"
+   #sg_ingress_rules = var.mongodb_sg_ingress_rules
+ }
 
 module "redis" {
   source         = "../../terraform-aws-securitygroup"
--- 02-sg/provider.tf
+++ 02-sg/provider.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "5.59.0"
     }
   }
 
 
   backend "s3" {
     bucket         = "chandudaws76-remote-state"
-    key            = "security-group-all"
+    key            = "securitygroup-all"
     region         = "us-east-1"
     dynamodb_table = "chandudaws76-locking"
 
