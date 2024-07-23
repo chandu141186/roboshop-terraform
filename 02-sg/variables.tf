3590968f2bfc86142e2d0e54d9b0c8844d7ee11e
chandu141186 <chandu141186@gmail.com>
2024-07-23 18:27:56 +0530

    terrafrom

--- 02-sg/variables.tf
+++ 02-sg/variables.tf
@@ -1,3 +1,15 @@
+variable "common_tags" {
+  default = {
+    Project     = "roboshop"
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+
+variable "sg_tags" {
+  default = {}
+}
+
 variable "project_name" {
     default = "roboshop"
   
@@ -5,4 +17,24 @@ variable "project_name" {
 variable "environment" {
     default = "dev"
   
+}
+
+
+variable "mongodb_sg_ingress_rules" {
+  default = [
+    {
+      description = "Allow port 80"
+      from_port   = 80 # 0 means all ports
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      description = "Allow port 443"
+      from_port   = 443 # 0 means all ports
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
 }
